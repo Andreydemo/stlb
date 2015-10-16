b3db3a97c53c55b4c3c9fc618e4e5422b816fd06
Andrii_Korkoshko <demosoft@bk.ru>
2015-10-16 18:25:00 +0300

    Js format

--- load_balancer/src/main/webapp/WEB-INF/js/main.js
+++ load_balancer/src/main/webapp/WEB-INF/js/main.js
@@ -1,92 +1,88 @@
-(function(){
+(function() {
     $('.ui.form')
         .form({
             fields: {
                 name: {
-                    rules: [
-                        {
-                            type: 'empty',
-                            prompt : 'Please enter a name'
-                        }
-                    ]
+                    rules: [{
+                        type: 'empty',
+                        prompt: 'Please enter a name'
+                    }]
                 },
                 url: {
-                    rules: [
-                        {
-                            type: 'empty',
-                            prompt : 'Please enter a url'
-                        },
-                        {
-                            type: 'regExp[]',
-                            prompt : 'Please enter a valid url'
-                        }
-                    ]
+                    rules: [{
+                        type: 'empty',
+                        prompt: 'Please enter a url'
+                    }, {
+                        type: 'regExp[]',
+                        prompt: 'Please enter a valid url'
+                    }]
                 }
             }
         });
-        $('.ui.accordion')
-          .accordion()
-        ;
-      /* $('.menu .item')
-         .tab()
-       ;*/
-        $('.menu .item')
-                .tab({
-                    cache: false,
-                    // faking api request
-                    apiSettings: {
-                      loadingDuration : 300,
-                      mockResponse    : function(settings) {
+    $('.ui.accordion')
+        .accordion();
+    /* $('.menu .item')
+       .tab()
+     ;*/
+    $('.menu .item')
+        .tab({
+            cache: false,
+            // faking api request
+            apiSettings: {
+                loadingDuration: 300,
+                mockResponse: function(settings) {
 
 
-                        var response = {
-                          first  : function(){
-                           $.ajax({
-                            url: '/stlb/nodeInfoFragment-' + $("#nodeId").val(),
-                             success: function(data) {
-                             $('#first-tab').html(data);
-                             }
-                            });
-                          },
-                          second : function(){
+                    var response = {
+                        first: function() {
                             $.ajax({
-                                   url: '/stlb/nodeLoading-' + $("#nodeId").val(),
-                                    success: function(data) {
+                                url: '/stlb/nodeInfoFragment-' + $("#nodeId").val(),
+                                success: function(data) {
+                                    $('#first-tab').html(data);
+                                }
+                            });
+                        },
+                        second: function() {
+                            $.ajax({
+                                url: '/stlb/nodeLoading-' + $("#nodeId").val(),
+                                success: function(data) {
                                     $('#second-tab').html(data);
-                                    }
-                                   });
-                                 },
+                                }
+                            });
+                        },
 
-                          third : function(){
-                           $.ajax({
-                                  url: '/stlb/cpuGraph-' + $("#nodeId").val(),
-                                   success: function(data) {
-                                   $('#third-tab').html(data);
-                                   }
-                                  });},
-                          fifth : function(){
-                             $.ajax({
-                                    url: '/stlb/cpuProcessGraph-' + $("#nodeId").val(),
-                                     success: function(data) {
-                                     $('#fifth-tab').html(data);
-                                     }
-                                    });},
-                            forth : function(){
-                                 $.ajax({
-                                        url: '/stlb/memoryGraph-' + $("#nodeId").val(),
-                                         success: function(data) {
-                                         $('#forth-tab').html(data);
-                                         }
-                                        });}
+                        third: function() {
+                            $.ajax({
+                                url: '/stlb/cpuGraph-' + $("#nodeId").val(),
+                                success: function(data) {
+                                    $('#third-tab').html(data);
+                                }
+                            });
+                        },
+                        fifth: function() {
+                            $.ajax({
+                                url: '/stlb/cpuProcessGraph-' + $("#nodeId").val(),
+                                success: function(data) {
+                                    $('#fifth-tab').html(data);
+                                }
+                            });
+                        },
+                        forth: function() {
+                            $.ajax({
+                                url: '/stlb/memoryGraph-' + $("#nodeId").val(),
+                                success: function(data) {
+                                    $('#forth-tab').html(data);
+                                }
+                            });
+                        }
 
 
-                        };
-                        return response[settings.urlData.tab];
-                      }
-                    },
-                    context : 'parent',
-                    auto    : true,
-                    path    : '/'
-                  })
-                ;
+                    };
+                    return response[settings.urlData.tab];
+                }
+            },
+            context: 'parent',
+            auto: true,
+            path: '/'
+        });
 })();
--- load_balancer/src/main/webapp/WEB-INF/js/memoryGraph.js
+++ load_balancer/src/main/webapp/WEB-INF/js/memoryGraph.js
@@ -1,105 +1,106 @@
-(function(){
-              $(document).ready(function () {
-               $.ajax({
-                      url: "",
-                      context: document.body,
-                      success: function(s,x){
+(function() {
+    $(document).ready(function() {
+        $.ajax({
+            url: "",
+            context: document.body,
+            success: function(s, x) {
 
-                          $('html[manifest=saveappoffline.appcache]').attr('content', '');
-                              $(this).html(s);
-                      }
-                  });
-                     Highcharts.setOptions({
-                         global: {
-                             useUTC: false
-                         }
-                     });
+                $('html[manifest=saveappoffline.appcache]').attr('content', '');
+                $(this).html(s);
+            }
+        });
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
 
-                     $('#containerMemory').highcharts({
-                         chart: {
-                             type: 'spline',
-                             animation: Highcharts.svg, // don't animate in old IE
-                             marginRight: 10,
-                             events: {
-                                 load: function () {
+        $('#containerMemory').highcharts({
+            chart: {
+                type: 'spline',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                events: {
+                    load: function() {
 
-                                     // set up the updating of the chart each second
-                                     var series = this.series[0];
-                                     setInterval(function () {
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        setInterval(function() {
 
-                                      $.ajax({
-                                         url: '/stlb/nodeLoading-' + $("#nodeId").val(),
-                                          async:false,
-                                         success: function(data) {
-                                              $(data).find('#first-report').each(function(index, el){
-                                                  var date = new Date($(el).find('.date').data('time')),
-                                                      cpu = (parseFloat($(el).find('.freemem').text()) /  parseFloat($(el).find('.totalmem').text()))*100;
-                                                      if(parseInt(series.processedXData[series.processedXData.length-1]) !== date.getTime()){
+                            $.ajax({
+                                url: '/stlb/nodeLoading-' + $("#nodeId").val(),
+                                async: false,
+                                success: function(data) {
+                                    $(data).find('#first-report').each(function(index, el) {
+                                        var date = new Date($(el).find('.date').data('time')),
+                                            cpu = (parseFloat($(el).find('.freemem').text()) / parseFloat($(el).find('.totalmem').text())) * 100;
+                                        if (parseInt(series.processedXData[series.processedXData.length - 1]) !== date.getTime()) {
 
-                                                       series.addPoint([date.getTime(), cpu], true, true);
-                                                       }
-                                              });
-                                          }
-                                      });
-                                     }, 1000);
-                                 }
-                             }
-                         },
-                         title: {
-                             text: 'Cpu loading'
-                         },
-                         xAxis: {
-                             type: 'datetime',
-                             tickPixelInterval: 150
-                         },
-                         yAxis: {
-                             title: {
-                                 text: 'Value'
-                             },
-                             plotLines: [{
-                                 value: 0,
-                                 width: 1,
-                                 color: '#808080'
-                             }]
-                         },
-                         tooltip: {
-                             formatter: function () {
-                                 return '<b>' + this.series.name + '</b><br/>' +
-                                     /*Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +*/
-                                     Highcharts.numberFormat(this.y, 2) + "%" + '<br/>' +
-                                     Highcharts.dateFormat('%H:%M:%S', this.x) ;
-                             }
-                         },
-                         legend: {
-                             enabled: false
-                         },
-                         exporting: {
-                             enabled: false
-                         },
-                         series: [{
-                             name: 'Cpu loading rate',
-                             data: (function () {
-
-                                 // generate an array of random data
-                                 var dataContainer = [],  i;
-                                    $.ajax({
-                                       url: '/stlb/nodeLoading-' + $("#nodeId").val(),
-                                       async:false,
-                                       success: function(data) {
-                                        var count = parseInt($(data).find("#reports-count").val())
-                                         for (i = count; i > 0; i--) {
-                                             var date = $($(data).find(".report-number-"+i)[0]).find('.date').data('time'),
-                                             cpu = (parseFloat($($(data).find(".report-number-"+i)[0]).find('.freemem').text()) / parseFloat($($(data).find(".report-number-"+i)[0]).find('.totalmem').text()))*100;
-                                             dataContainer.push({
-                                                 x: new Date(date).getTime(),
-                                                 y: cpu
-                                             });
-                                         }
+                                            series.addPoint([date.getTime(), cpu], true, true);
                                         }
                                     });
-                                 return dataContainer;
-                             }())
-                         }]
-                     });
-                 });
+                                }
+                            });
+                        }, 1000);
+                    }
+                }
+            },
+            title: {
+                text: 'Cpu loading'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function() {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        /*Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +*/
+                        Highcharts.numberFormat(this.y, 2) + "%" + '<br/>' +
+                        Highcharts.dateFormat('%H:%M:%S', this.x);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'Cpu loading rate',
+                data: (function() {
+
+                    // generate an array of random data
+                    var dataContainer = [],
+                        i;
+                    $.ajax({
+                        url: '/stlb/nodeLoading-' + $("#nodeId").val(),
+                        async: false,
+                        success: function(data) {
+                            var count = parseInt($(data).find("#reports-count").val())
+                            for (i = count; i > 0; i--) {
+                                var date = $($(data).find(".report-number-" + i)[0]).find('.date').data('time'),
+                                    cpu = (parseFloat($($(data).find(".report-number-" + i)[0]).find('.freemem').text()) / parseFloat($($(data).find(".report-number-" + i)[0]).find('.totalmem').text())) * 100;
+                                dataContainer.push({
+                                    x: new Date(date).getTime(),
+                                    y: cpu
+                                });
+                            }
+                        }
+                    });
+                    return dataContainer;
+                }())
+            }]
+        });
+    });
 })();
